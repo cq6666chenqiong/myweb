17aa000bf3d3454505e6956965bf0cfe773ff513
chenqiong <4444@163.com>
2018-02-26 13:47:06 +0800

    test

--- src/main/java/com/cn/myweb/Test.java
+++ src/main/java/com/cn/myweb/Test.java
@@ -6,23 +6,33 @@ package com.cn.myweb;
 public class Test {
 
     public void te(){
-
+        if(1==2){
+            System.out.println("f2ds");
+        }
     }
 
     public void tdd(){
-
+        if(1==2){
+            System.out.println("fds");
+        }
     }
 
     public void td(){
-
+        if(1==1){
+            System.out.println("fds");
+        }
     }
 
     public void td1(){
-
+        if(1==1){
+            System.out.println("fds");
+        }
     }
     
     public void ssd(){
-        
+         if(1==2){
+            System.out.println("fd2s");
+        }
     }
 
 }
