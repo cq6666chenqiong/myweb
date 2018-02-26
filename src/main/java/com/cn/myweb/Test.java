40101b55e1d38b2fd6e4faf11ff46d282d2779a2
cq6666chenqiong <498172399@qq.com>
2018-02-26 11:26:42 +0800

    dwf
    ff

--- src/main/java/com/cn/myweb/Test.java
+++ src/main/java/com/cn/myweb/Test.java
@@ -6,18 +6,26 @@ package com.cn.myweb;
 public class Test {
 
     public void te(){
-
+ if(1==2){
+            System.out.println("f2ds");
+        }
     }
 public void tdd(){
-
+ if(1==2){
+            System.out.println("2fds");
+        }
 }
     
     public void ddd(){
-        
+         if(1==2){
+            System.out.println("fds2");
+        }
     }
     
     public void ssd(){
-        
+         if(1==2){
+            System.out.println("fd2s");
+        }
     }
 
 }
