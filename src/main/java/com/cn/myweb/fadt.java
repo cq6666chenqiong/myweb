df79baff4b0840ec790639904b9b5bfdfe041122
cq6666chenqiong <498172399@qq.com>
2018-02-27 09:27:43 +0800

    111
    111

--- src/main/java/com/cn/myweb/fadt.java
+++ src/main/java/com/cn/myweb/fadt.java
@@ -4,4 +4,9 @@ package com.cn.myweb;
  * Created by chenqiong on 2018/2/26.
  */
 public class fadt {
+  public void thisMyWay(){
+        System.out.println("this my way0");
+        System.out.println("this my way12");
+        System.out.println("this my way22");
+    }
 }
